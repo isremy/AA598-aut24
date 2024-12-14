efac05fe30949348723af4d105cf68ceb124e9bb
Isaac Remy <iremy@uw.edu>
2024-12-13 19:15:53 -0800

    hw3

--- aa598/hw3_helper.py
+++ aa598/hw3_helper.py
@@ -14,4 +14,4 @@ def plot_value_function(i, j, k, values, grid):
                 colors="black",
                 linewidths=3)
 
-    plt.title("angle: %.2f deg, v_rob: %.2f, v_hum: %.2f"%(grid.coordinate_vectors[2][i] * 180 / jnp.pi, grid.coordinate_vectors[3][j], grid.coordinate_vectors[4][k]))
+    plt.title("angle: %.2f deg, v_rob: %.2f, v_cat: %.2f"%(grid.coordinate_vectors[2][i] * 180 / jnp.pi, grid.coordinate_vectors[3][j], grid.coordinate_vectors[4][k]))
